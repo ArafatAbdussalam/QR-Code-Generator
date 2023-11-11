75beb6503300086b456971f27ed90a9fefe2ae79
Arafat Abdussalam <abdussalamarafat@gmail.com>
2023-11-11 21:58:18 +0100

    fix: restructure src folder and remove favicon from index file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+import "./App.css"
+
+const App = () => {
+
+
+    return (
+        <Fragment>
+            
+        </Fragment>
+    )
+}
+
+export default App;
