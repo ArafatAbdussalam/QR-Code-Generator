a292be22a5efb3f70555879636f853f86a384608
Arafat Abdussalam <abdussalamarafat@gmail.com>
2023-11-30 19:52:04 +0100

    fix: rewrite logic for QrButtonField

--- src/components/QrField/QrButton/QrButtonField.jsx
+++ src/components/QrField/QrButton/QrButtonField.jsx
@@ -1,12 +1,32 @@
+import { useContext } from "react";
+
+import "./QrButtonField.css"
+
 import Button from "../../UI/Button/Button";
 import QrFieldData from "../QrData/QrFieldData";
+import AuthContext from "../../store/auth-context";
+import QrInputField from "../QrInput/QrInputField";
+
 
 const QrButtonField = () => {
+
+    const context = useContext(AuthContext)
 
     const QrButtons = QrFieldData
 
     const showQrInputFieldHandler = () => {
+
+        if (!context.isLoggedIn) {
+            return
+            // works for authenticated users only
+        }
         
+        if (context.isLoggedIn) {
+            return (
+                <QrInputField />
+            )
+
+        }
     }
 
     QrButtons.map(
