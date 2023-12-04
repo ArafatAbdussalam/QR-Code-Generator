57ecb5c91881017c3d87e215a354d58469def139
Arafat Abdussalam <abdussalamarafat@gmail.com>
2023-12-04 16:36:10 +0100

    fix: add logic for linking button id with input field id when button is clicked

--- src/components/QrField/QrButton/QrButtonField.jsx
+++ src/components/QrField/QrButton/QrButtonField.jsx
@@ -8,6 +8,12 @@ const QrButtonField = (props) => {
 
     const {onShowQrInputField} = props
 
+    const showQrInputFieldHandler = () => {
+
+        // set buttonId such that it links to the kind of input field to be displayed when clicked
+        onShowQrInputField()
+    }
+
     const qrData = QrFieldData
 
     const QrButtons = qrData.map(
@@ -16,7 +22,7 @@ const QrButtonField = (props) => {
                 const buttonText = item.text
 
                 return (
-                    <Button key={item.id} onClick={onShowQrInputField}>{buttonText}</Button>
+                    <Button key={item.id} onClick={showQrInputFieldHandler}>{buttonText}</Button>
                 )
             }
         )
